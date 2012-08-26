1a68d400bca72fd2425fc4e312e2c987914a6c37
Your Name <phyten.obr@gmail.com>
2012-08-26 22:19:56 +0900

    add_music.rb

--- app/models/music.rb
+++ app/models/music.rb
@@ -18,4 +18,30 @@ class Music < ActiveRecord::Base
       end
     end
   end
+
+  def self.add_music(artist_name, music_name)
+    youtube = Youtube.new
+    good_musics = []
+
+    Artist.transaction do
+      pp good_music = youtube.search_keyword([artist_name, music_name], music_name).good_music
+      artist_id = ""
+      if Artist.find_by_name_downcase(artist_name.downcase).blank?
+        Artist.create({:name => artist_name, :name_downcase => artist_name.downcase})
+        artist_id = Artist.find(:last).id
+      else
+        artist_id = Artist.find_by_name_downcase(artist_name.downcase).id
+      end
+      music_id = ""
+      if Music.find_by_name_downcase(music_name.downcase).blank?
+        Music.create({:name => music_name, :name_downcase => music_name.downcase, :artist_id => artist_id, :addr => good_music[:addr]})
+        music_id = Music.find(:last).id
+      else          
+        music = Music.find_by_name_downcase(music_name.downcase)
+        music.addr = good_music[:addr]
+        music.save!
+        music_id = Music.find_by_name_downcase(music_name.downcase).id
+      end
+    end                         # end of transaction
+  end
 end
---
+++ script/add_music.rb
@@ -0,0 +1,3 @@
+Music.add_music("ゆず", "サヨナラバス")
+Music.add_music("ゆず", "夏色")
+Music.add_music("いきものがかり", "気まぐれロマンティック")
